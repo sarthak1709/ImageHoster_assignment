ef7b60aca14e8e3b0434c4114bc59866d401f73b
Suman P S <suman.bgl@gmail.com>
2019-04-07 13:36:56 +0530

    test branch commit

--- ImageHoster/src/main/java/ImageHoster/controller/UserController.java
+++ ImageHoster/src/main/java/ImageHoster/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
-
+/*Adding test comment */
 @Controller
 public class UserController {
 
